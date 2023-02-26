dbdf8948dd722e37cce99045531a85070a2f1ff6
Ryuku <123806438+Ryuku999@users.noreply.github.com>
2023-02-26 12:46:22 +0100

    Nozzle Max Temp error

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -620,7 +620,7 @@
 // Above this temperature the heater will be switched off.
 // This can protect components from overheating, but NOT from shorts and failures.
 // (Use MINTEMP for thermistor short/failure protection.)
-#define HEATER_0_MAXTEMP 300 //Seb
+#define HEATER_0_MAXTEMP 290 //Seb
 #define HEATER_1_MAXTEMP 275
 #define HEATER_2_MAXTEMP 275
 #define HEATER_3_MAXTEMP 275
